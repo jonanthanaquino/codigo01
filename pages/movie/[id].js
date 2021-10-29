d443f83422aa278f3868cca631d46abc32f8e5c5
jonanthanaquino <jonanthan.aquino27@gmail.com>
2021-10-29 14:19:16 -0400

    Update [id].js

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -39,7 +39,7 @@ export default function MovieItem({info}) {
    
 
 export async function getServerSideProps(context){
-  const res= await fetch(`http://localhost:3000/api/movie/${context.params.id}`);
+  const res= await fetch(`http://codigo01-k6f27dpo0-equipe-1.vercel.app/api/movie/${context.params.id}`);
   const json= await res.json();
   return {
     props:{
